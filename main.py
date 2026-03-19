bd1a0298cbbd1793d3e22cc0cb495ce8819638c9
Jon Bergland <jobergl@stud.ntnu.no>
2026-03-19 12:17:15 +0100

    refactor: create sepearate parameters for ViT and Resnet

--- main.py
+++ main.py
@@ -17,54 +17,33 @@ def main(dataset_root: str,
          lr_step_size: int = 5,
          lr_gamma: float = 0.2):
 
-    # cnn = ResnetTrainer(dataset_root=dataset_root, 
-    #                     model_name=model_name,
-    #                     epochs = epochs,
-    #                     lr_rate = lr_rate,
-    #                     batch_size = batch_size,
-    #                     img_size = img_size,
-    #                     manual_seed = manual_seed,
-    #                     save_path=save_path,
-    #                     only_see_metrics=only_see_metrics,
-    #                     dropout_rate=dropout_rate,
-    #                     label_smoothing=label_smoothing,
-    #                     weight_decay=weight_decay,
-    #                     lr_step_size=lr_step_size,
-    #                     lr_gamma=lr_gamma)
+    model_name_lower = model_name.lower()
+    trainer_class = VisionTransformerTrainer if "vit" in model_name_lower else ResnetTrainer
 
-    
+    trainer = trainer_class(
+        dataset_root=dataset_root,
+        model_name=model_name,
+        epochs=epochs,
+        lr_rate=lr_rate,
+        batch_size=batch_size,
+        img_size=img_size,
+        manual_seed=manual_seed,
+        save_path=save_path,
+        only_see_metrics=only_see_metrics,
+        dropout_rate=dropout_rate,
+        label_smoothing=label_smoothing,
+        weight_decay=weight_decay,
+        lr_step_size=lr_step_size,
+        lr_gamma=lr_gamma,
+    )
 
-    # cnn.train()
-    # cnn.evaluate()
+    trainer.train()
+    trainer.evaluate()
 
-    # cnn.save_model(model=cnn.model, save_optimizer=True)
-    # cnn.clear_model()
+    trainer.save_model(model=trainer.model, save_optimizer=True)
+    trainer.clear_model()
 
-    # cnn.plot_metrics()
-
-    vit = VisionTransformerTrainer(dataset_root=dataset_root, 
-                        model_name=model_name,
-                        epochs = epochs,
-                        lr_rate = lr_rate,
-                        batch_size = batch_size,
-                        img_size = img_size,
-                        manual_seed = manual_seed,
-                        save_path=save_path,
-                        only_see_metrics=only_see_metrics,
-                        dropout_rate=dropout_rate,
-                        label_smoothing=label_smoothing,
-                        weight_decay=weight_decay,
-                        lr_step_size=lr_step_size,
-                        lr_gamma=lr_gamma)
-    
-    
-    vit.train()
-    vit.evaluate()
-
-    vit.save_model(model=vit.model, save_optimizer=True)
-    vit.clear_model()
-
-    vit.plot_metrics()
+    trainer.plot_metrics()
 
     
 if __name__ == '__main__':
@@ -73,38 +52,48 @@ if __name__ == '__main__':
     save_path = os.path.join(BASE_DIR, "saved_models")
     model_name = "vit_no_data_augmentation.pth"
 
-    epochs = 1
-    lr_rate = 1e-4
+    epochs = 30
     batch_size = 32
     img_size = 128
     manual_seed = 42
     only_see_metrics = False
 
-    
-    # Overfitting measures
-    dropout_rate = 0.6
-    label_smoothing = 0.05
-    weight_decay = 2e-3
-
-    # Learning rate
-    lr_step_size = 5
-    lr_gamma = 0.2
+    if "resnet" in model_name.lower():
+        # ResNet settings
+        dropout_rate = 0.6
+        label_smoothing = 0.05
+        weight_decay = 2e-3
+        lr_rate = 1e-3
+        lr_step_size = 5
+        lr_gamma = 0.2
+    elif "vit" in model_name.lower():
+        # ViT settings
+        dropout_rate = 0.2
+        label_smoothing = 0.1
+        weight_decay = 1e-2
+        lr_rate = 3e-4
+        lr_step_size = 10
+        lr_gamma = 0.5
+    else:
+        raise ValueError(
+            "model_name must include either 'resnet' or 'vit' to choose model-specific parameters"
+        )
 
     
 
 
     main(dataset_root=root,
-         model_name=model_name,
-         epochs= epochs,
-         lr_rate= lr_rate,
-         batch_size=batch_size,
-         img_size=img_size,
-         manual_seed=manual_seed,
-         save_path=save_path,
-            only_see_metrics=only_see_metrics,
-            dropout_rate=dropout_rate,
-            label_smoothing=label_smoothing,
-            weight_decay=weight_decay,
-            lr_step_size=lr_step_size,
-            lr_gamma=lr_gamma)
+        model_name=model_name,
+        epochs= epochs,
+        lr_rate= lr_rate,
+        batch_size=batch_size,
+        img_size=img_size,
+        manual_seed=manual_seed,
+        save_path=save_path,
+        only_see_metrics=only_see_metrics,
+        dropout_rate=dropout_rate,
+        label_smoothing=label_smoothing,
+        weight_decay=weight_decay,
+        lr_step_size=lr_step_size,
+        lr_gamma=lr_gamma)
 
